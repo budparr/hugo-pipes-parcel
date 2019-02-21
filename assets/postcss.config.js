b3e2199d84c6ed0a6d17973a317def0d1e26a8ac
budparr <budparr@gmail.com>
2019-02-21 08:53:49 -0500

    Improved postcss.config.js to work with either Hugo's Pipes for CSS processing, or via Parcel.

---
+++ assets/postcss.config.js
@@ -0,0 +1,32 @@
+//console.log("NODE_ENV", process.env.NODE_ENV);
+module.exports = {
+	plugins: [
+		require("postcss-import")({
+			path: ["./assets/css"]
+		}),
+		require("tailwindcss")("./assets/css/tailwindcss/tailwind.config.js"),
+		require("autoprefixer")({
+			grid: false,
+			browsers: [">1%"]
+		}),
+		...(process.env.NODE_ENV !== "development"
+			? [
+					require("@fullhuman/postcss-purgecss")({
+						content: ["./layouts/**/*.html", "./components/**/**/*.html"],
+						extractors: [
+							{
+								extractor: class {
+									static extract(content) {
+										//console.log(process.env.NODE_ENV);
+										//return content.match(/[a-zA-Z0-9-:_/]+/g) || [];
+										return content.match(/[A-z0-9-:\/]+/g) || [];
+									}
+								},
+								extensions: ["vue", "html"]
+							}
+						]
+					})
+			  ]
+			: []) //If Development, do not use PurgeCSS
+	]
+};
